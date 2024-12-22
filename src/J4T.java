9b73c4c2ae1fc0651e5287133d4a6f687cd96597
k1lyabenzo <georgy-zhogov@yandex.kz>
2024-12-22 21:42:20 +0300

    1

---
+++ src/J4T.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class J4T {
+    //Массив кислот
+    static String[] acids = {"HBr", "HCH3COO", "HCO3", "HCl", "HF", "HI", "HNO3", "HNO2", "HPO4",
+            "H2S", "HSO3", "HSO4", "H2SiO3", "HCN", "H3AsO4", "H2CrO4", "H2Cr2O7", "HMnO4", "HClO",
+            "HClO2", "HClO3", "HClO4", "HOOCH", "H2C2O4", "HAlO2", "HAsO3", "HBO2", "H3BO3", "H2B4O7",
+            "HBrO", "HBrO3"};
+    //Массив кислотных остатков
+    static String[] acid_salts = {"Br", "C3COO", "CO3", "Cl", "F", "I", "NO3", "NO2", "PO4",
+            "2S", "SO3", "SO4", "2SiO3", "CN", "3AsO4", "2CrO4", "2Cr2O7", "MnO4", "ClO",
+            "ClO2", "ClO3", "ClO4", "OOC", "2C2O4", "AlO2", "AsO3", "BO2", "3BO3", "2B4O7",
+            "BrO", "BrO3"};
+    //Массив оснований
+    static String[] bases = {"OH"};
+}
